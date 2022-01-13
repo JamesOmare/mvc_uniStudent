3838c8cec55cca138107a2ea1a3e50295d5351c5
JamesOmare <jamesomare922@gmail.com>
2022-01-14 00:41:59 +0300

    Add project files.

---
+++ MyUniversity/Models/University.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace MyUniversity.Models
+{
+    public class University
+    {
+        public int ID { get; set; }
+        public string Institute { get; set; }
+        public DateTime dateofreporting { get; set; }
+        public string Course { get; set; }
+        public string Department { get; set; }
+        public decimal year { get; set; }
+    }
+
+    public class UniversityDBContext : DbContext
+    {
+        public DbSet<University> Universities { get; set; }
+    }
+
+}
